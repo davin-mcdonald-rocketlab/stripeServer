c928452618d2e3f1373243d18f2469b0ff112798
Davin McDonald <davin.mcdonald@rocketlab.com.au>
2023-11-01 12:55:08 +1100

    add try catch and env sample

--- server.js
+++ server.js
@@ -10,27 +10,35 @@ app.use(express.json());
 
 app.post('/payment-sheet', async (req, res) => {
   // Use an existing Customer ID if this is a returning customer.
-  const customer = await stripe.customers.create();
-  const ephemeralKey = await stripe.ephemeralKeys.create(
-    { customer: customer.id },
-    { apiVersion: '2023-08-16' }
-  );
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1099,
-    currency: 'aud',
-    customer: customer.id,
-    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+  const customerDetails = req.body.customerDetails;
+  try {
+    const customer = await stripe.customers.create(customerDetails);
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { apiVersion: '2023-08-16' }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: customerDetails.price,
+      currency: 'aud',
+      customer: customer.id,
+      // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
 
-  res.json({
-    paymentIntent: paymentIntent.client_secret,
-    ephemeralKey: ephemeralKey.secret,
-    customer: customer.id,
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY, // Replace 'YourPublicKey' with your actual Stripe public key.
-  });
+    console.warn('paymentIntent', paymentIntent)
+  
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY, // Replace 'YourPublicKey' with your actual Stripe public key.
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 });
 
 app.listen(port, () => {
